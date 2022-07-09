26e220142d5ab81f47e5cbe11458ebdf48d61ee7
Luzan Baral <luzanbaral@gmail.com>
2022-07-08 22:34:02 -0500

    repository created

---
+++ src/main/java/edu/miu/cs/cs425/eregistrarwebapi/repository/StudentRepository.java
@@ -0,0 +1,10 @@
+package edu.miu.cs.cs425.eregistrarwebapi.repository;
+
+import edu.miu.cs.cs425.eregistrarwebapi.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+}
